03955316bce962c55710872c2b587f392b247f79
Kristan <ksamaroo@uoguelph.ca>
2022-03-06 15:01:00 -0500

    added grammar defs

--- absyn/Type.java
+++ absyn/Type.java
@@ -1,5 +1,13 @@
 package absyn;
 
 public class Type {
-    
+    public static int VOID = 0; 
+    public static int INT = 1; 
+
+    public int type; 
+
+    public Type(int type, int pos) {
+        this.type = type
+        this.pos = pos
+    }
 }
