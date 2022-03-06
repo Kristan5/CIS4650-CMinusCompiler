11e4758fca430397322c1934ecf80ea8942de85f
Kristan <ksamaroo@uoguelph.ca>
2022-03-06 18:07:21 -0500

    Fixed errors

--- absyn/ArrayDec.java
+++ absyn/ArrayDec.java
@@ -1,5 +1,12 @@
 package absyn;
 
 public class ArrayDec extends VarDec {
-    
+    public IntExp size;
+    public int pos; 
+	public ArrayDec(int pos, Type type, String name, IntExp size){
+		this.pos = pos;
+		this.type = type;
+		this.name = name;
+		this.size = size;
+	}  
 }
--- absyn/SimpleDec.java
+++ absyn/SimpleDec.java
@@ -1,5 +1,10 @@
 package absyn;
 
-public class SimpleDec {
-    
+public class SimpleDec extends VarDec {
+    public int pos;
+    public SimpleDec(int pos, Type type, String name){
+		this.pos = pos;
+		this.type = type;
+		this.name = name;
+	}
 }
