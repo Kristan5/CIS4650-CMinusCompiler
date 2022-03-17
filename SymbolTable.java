637f6dae6548e17832e1ba22420c11556f190b6c
Kristan <ksamaroo@uoguelph.ca>
2022-03-16 21:20:01 -0400

    Added call to analyzer in .cup

--- SymbolTable.java
+++ SymbolTable.java
@@ -2,7 +2,10 @@ import absyn.*;
 import symbol.*; 
 
 public class SymbolTable {
+    private ArrayList<HashMap<Integer, Symbol>> symbolTable; 
+
     public SymbolTable() {
-        
+        symbolTable = new ArrayList<HashMap<Integer, Symbol>>();
+
     }
 }
