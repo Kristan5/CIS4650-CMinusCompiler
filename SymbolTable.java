eba46cfc6ae553c10c34000c1874c00560d33aaf
Indeep Farma <ifarma@uoguelph.ca>
2022-03-17 22:52:45 -0400

    Starting symbolTable

--- SymbolTable.java
+++ SymbolTable.java
@@ -1,14 +1,57 @@
 import absyn.*; 
 import symbol.*; 
 
-import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SymbolTable {
-    private ArrayList<HashMap<Integer, Symbol>> symbolTable; 
+    private ArrayList<HashMap<Integer, Symbol>> symbolTable;
+    // private boolean scope;
+
+    final static int SPACES = 4;
 
     public SymbolTable() {
         symbolTable = new ArrayList<HashMap<Integer, Symbol>>();
 
     }
+
+    // Make indentation
+    private String indent(int level) {
+        for( int i = 0; i < level * SPACES; i++ ) {
+            System.out.print( " " );   
+        }
+    }
+
+    // Get the type
+    // private String getType(int type) {
+    //     if(type == Type.VOID) {
+    //         return "VOID";
+    //     } else {
+    //         return "INT";
+    //     }
+    // }
+
+    // Add a new scope by making a new hashmap
+    // Add it to the arraylist
+    public void newScope() {
+        symbolTable.add(new HashMap<Integer, Symbol>());
+    }
+
+    // Delete and exit the scope you're in
+    public void delCurrScope() {
+        int tableLength = symbolTable.size();
+
+        // Make sure there's items in list
+        if(tableLength > 0) {
+            symbolTable.remove(tableLength - 1);
+        }
+    }
+
+    // Add a symbol to the hashmap
+    public void addSymbol(int id, Symbol symb) {
+        int length = symbolTable.size();
+        symbolTable.get(length - 1).put(id, symb);
+    }
+
+
 }
